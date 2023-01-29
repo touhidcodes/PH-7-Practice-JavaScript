aaa5ddfff7b77b66cbaed767c92924b1d3f9d178
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-29 20:27:34 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,96 @@
+// 1. What is JavaScript?
+/* 
+JavaScript is a interpreted scripting programming language used primarily to create interactive front-end web applications, that interaction with website can change dynamically website in browser.
+*/
+
+// 2. How to work JavaScript?
+/* 
+ To work with JavaScript, one can use a text editor to write code and a web browser to run and test the code. JavaScript code can be added to an HTML document using the <script> tag. It can also be run on the server-side using technologies such as Node.js.
+ */
+
+//  3. What is Variable?
+/* 
+A JavaScript variable is simply a name of storage location. 
+*/
+
+// 4. How to declare variable?
+/* 
+a. write var for variable statement
+b. write variable name
+c. use equal (=)  as a assign operator
+d. write the value of variable
+e. use semicolon (;) as end of the variable
+
+var numbers = 23;
+*/
+
+// 5. How to change or update variable value?
+/* 
+write variable name and use equal (=) then put nuw value of the variable
+*/
+
+// 6. Types o variables?
+/* 
+a. number
+b. boolean
+c. string
+d. array
+e. object
+f. bigInt
+*/
+
+// 7. Primitive and Non-Primitive value of js?
+/* 
+primitive:
+number
+b. boolean
+c. string
+d. undefined
+e. null
+
+Non-Primitive:
+a. array
+b. object
+c. functions
+*/
+
+// 8. what are the variable naming convention?
+/* 
+a. use camel case or underscore
+b. don't use dash
+c. don't use and keywords
+d. variable name should be in single word
+e. variable name can't starts with number but can end with numbers
+f. variable name can't have quotation
+*/
+
+// 9. JS Math Operators?
+/* 
+equal = =
+addition = +
+subtraction = -
+multiplication = *
+division = /
+modulus/ reminder = %
+*/
+
+// 10. JS shorthands?
+/* 
++=: x = x + 1
+-=: x = x - 1
+*=: x = x * 1
+/=: x = x / 1
+*/
+
+// 11. Increment and Decrement?
+/* 
+++: Increase value + 1
+--: Decrease value - 1
+*/
+
+// 12. parseInt, parseFloat, toFixed?
+/* 
+parseInt(): parse string to numbers to integer
+ parseFloat(): parse string to numbers to float
+  toFixed(): Fixes the value of number to decimal point
+*/
