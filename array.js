4c3a228212a715332cce5104411ca0d64afed0c5
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-04 17:38:25 +0600

    practice-solving

---
+++ array.js
@@ -0,0 +1,54 @@
+// 13. What is array? How to declare array?
+/* 
+Array is c container of one or more values. Values can be number, string, object, boolean.
+
+const array = [12, 23, 45];
+*/
+
+// 14. How to find elements of array?
+/* 
+array.length
+*/
+
+// 15. how to find array element position?
+/* 
+array.index[0];
+array index starts at 0
+*/
+
+// 16. Array inch -1 means what?
+/* 
+It means the element of the index is not exist in the array
+*/
+
+// 17. How to find array index of element?
+/* 
+array.index[0];
+*/
+
+// 18. How to find array index of element?
+/* 
+array.index[0] = "value"
+*/
+
+// 19. How to find index from array element?
+/* 
+const value = array.indexOf["element"]
+*/
+
+// 20. What is the means of "undefined" in array?
+/* 
+Undefined value primitive value is used when a variable has not been assigned a value
+*/
+
+// 21. How to add or remove element in last position of array?
+/* 
+add: push()
+remove: pop()
+*/
+
+// 22. How to add or remove element in first position of array?
+/* 
+add: unshift()
+remove: shift()
+*/
---
+++ conditionals.js
@@ -0,0 +1,28 @@
+// 23. What are the means of: >, <, ==, !=, <=, >=, ===, !==, &&, ।। ?
+/* 
+> : greater than
+< : smaller then
+== : equal
+!= : not equal
+<= : smaller then or equal
+>= : greater then or equal
+=== : equal and equal in types
+!== : not equal and not equal in types
+&& : logical and
+।। : logical or
+*/
+
+// 24. If i have more than 80k i will buy mac, more than 60k : gaming laptop, more than 40k : lenovo laptop, more than 20k : reused laptop, and less than 20k i will buy a phone.
+
+const myMoney = 56000;
+if (myMoney >= 80000) {
+	console.log("mac laptop");
+} else if (myMoney >= 60000) {
+	console.log("gaming laptop");
+} else if (myMoney >= 40000) {
+	console.log("lenovo laptop");
+} else if (myMoney >= 20000) {
+	console.log("used Laptop");
+} else {
+	console.log("phone");
+}
